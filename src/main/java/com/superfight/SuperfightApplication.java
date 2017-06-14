f41d8fb3e34863be67e6738d44cecd348d71b462
Conor <conorhaworth4@gmail.com>
2017-06-14 07:49:09 +0100

    Project skeleton

---
+++ src/main/java/com/superfight/SuperfightApplication.java
@@ -0,0 +1,12 @@
+package com.superfight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperfightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperfightApplication.class, args);
+	}
+}
